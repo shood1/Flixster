a373fd0af007aedd591bc4cdb8db9315372c1477
cs_apple_10 <cs_apple_10@csapple10simac.aamu.edu>
2019-10-24 13:57:46 -0500

    Flixster application by Steven Hood

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Steven
+//  Copyright © 2019 . All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Job Albarr on 2/6/19.
+//  Copyright © 2019 Job Albarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
